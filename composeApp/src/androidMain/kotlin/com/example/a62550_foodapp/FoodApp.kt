daa134662f28f3a724cd9df0e31503a936eef8b4
s163961 <s163961@student.dtu.dk>
2026-01-05 13:20:50 +0100

    fixed?

--- composeApp/src/androidMain/kotlin/com/example/a62550_foodapp/FoodApp.kt
+++ composeApp/src/androidMain/kotlin/com/example/a62550_foodapp/FoodApp.kt
@@ -1,4 +1,17 @@
 package com.example.a62550_foodapp
 
-class FoodApp {
+import android.app.Application
+import com.example.a62550_foodapp.di.initKoin
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+
+class FoodApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        initKoin {
+            androidLogger()
+            androidContext(this@FoodApp)
+        }
+    }
 }
--- composeApp/src/iosMain/kotlin/com/example/a62550_foodapp/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/com/example/a62550_foodapp/MainViewController.kt
@@ -2,4 +2,6 @@ package com.example.a62550_foodapp
 
 import androidx.compose.ui.window.ComposeUIViewController
 
+import com.example.a62550_foodapp.di.initKoin
+
 fun MainViewController() = ComposeUIViewController { App() }
