884894041556ee2f1b95997718918b00c1c69b4a
ih8mondays <bjoerkobel@hotmail.dk>
2026-01-05 14:53:50 +0100

    merged Koin into ViewModel - fix fixed

--- composeApp/src/androidMain/kotlin/com/example/a62550_foodapp/di/AppModule.kt
+++ composeApp/src/androidMain/kotlin/com/example/a62550_foodapp/di/AppModule.kt
@@ -1,8 +1,8 @@
 package com.example.a62550_foodapp.di
 
 import com.example.a62550_foodapp.viewmodel.AndroidMainViewModel
-import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
+import org.koin.core.module.dsl.*
 
 val appModule = module {
     viewModel { AndroidMainViewModel() }
